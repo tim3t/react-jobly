118bcf136f8f75d6fcf3ceddc16d4170f7a4999b
Timothy Threet <timothythreet@Timothys-MacBook-Pro.local>
2023-01-04 14:24:51 -0500

    completed except for styling

---
+++ build/precache-manifest.257e625e0a94ab97943e030c35f1d2b9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f91edada7e9ed1e906a5858f818fe6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "526caa1346cc88fc1261",
+    "url": "/static/css/2.560b9325.chunk.css"
+  },
+  {
+    "revision": "60b7d0f5ea1992c5ad2b",
+    "url": "/static/css/main.85068bc6.chunk.css"
+  },
+  {
+    "revision": "526caa1346cc88fc1261",
+    "url": "/static/js/2.23f594c0.chunk.js"
+  },
+  {
+    "revision": "395cc4b3602716a6dc8075912d30f948",
+    "url": "/static/js/2.23f594c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60b7d0f5ea1992c5ad2b",
+    "url": "/static/js/main.0c5812bf.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.0c5812bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e157b3318261263bb32a",
+    "url": "/static/js/runtime-main.ca09d11f.js"
+  }
+]);
--- src/api/api.js
+++ src/api/api.js
@@ -2,14 +2,6 @@ import axios from 'axios';
 
 const BASE_URL = process.env.REACT_APP_BASE_URL || 'http://localhost:3001';
 
-/** API Class.
- *
- * Static class tying together methods used to get/send to to the API.
- * There shouldn't be any frontend-specific stuff here, and there shouldn't
- * be any API-aware stuff elsewhere in the frontend.
- *
- */
-
 class JoblyApi {
 	// the token for interactive with the API will be stored here.
 	static token;
